c97dd466f8c6acb40756587b4d25a21fd247a74e
olliebuddy <ocoucke@parrastu.catholic.edu.au>
2025-04-03 09:48:37 +1100

    Week 4 Done

--- pwa_project/settings.py
+++ pwa_project/settings.py
@@ -30,16 +30,40 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
+SITE_ID = 1
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+)
+
+INSTALLED_APPS = [
+    'users',
+    'tasks',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.github',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'allauth.account.middleware.AccountMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+   
 ]
 
 ROOT_URLCONF = 'pwa_project.urls'
@@ -115,13 +139,31 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-INSTALLED_APPS = [
-    'users',
-    'tasks',
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-]
+SOCIALACCOUNT_PROVIDERS = {
+    'github': {
+        'SCOPE': [
+            'user',
+            'repo',
+            'read:user',
+        ],
+    }
+}
+
+LOGIN_REDIRECT_URL = '/users/'
+LOGOUT_REDIRECT_URL = '/users/login/'
+
+SESSION_COOKIE_AGE = 1209600  # Two weeks
+SESSION_SAVE_EVERY_REQUEST = True  # Save session with each request
+
+SOCIALACCOUNT_PROVIDERS = {
+    'github': {
+        'SCOPE': [
+            'user',
+            'repo',
+            'read:user',
+        ],
+    }
+}
+
+SESSION_COOKIE_AGE = 1209600  # Two weeks
+SESSION_SAVE_EVERY_REQUEST = True  # Save session with each request
--- pwa_project/urls.py
+++ pwa_project/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin 
+from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [ 
-	path('admin/', admin.site.urls), 
-	path('users/', include(("users.urls", "users"), namespace="users")),
-	path('tasks/', include(("tasks.urls", "tasks"), namespace="tasks")),
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('users/', include(("users.urls", "users"), namespace="users")),
+    path('tasks/', include(("tasks.urls", "tasks"), namespace="tasks")),
+    path('accounts/', include('allauth.urls')),
 ]
